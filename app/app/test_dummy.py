f8307711e242c723621541437100bd1a77c65caf
Ipolitas <i.janeliunas@gmail.com>
2024-04-29 15:29:38 +0300

    added api/docs (swagger)

--- app/app/test_dummy.py
+++
@@ -1,5 +0,0 @@
-# this is a dummy test file to create at least one test case for CI/CD pipeline
-# Otherwise pipeline would fail because "no tests were found"
-
-def test_dummy():
-    assert 1 == 1
